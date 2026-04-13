03e7db5b64b59331dcc49a434e6c5fa5c994fdcd
haonan <ghn108@163.com>
2026-04-13 14:59:23 +0800

    feat: 增加羊了个羊难度到16*15

--- components/games/sheep-tiles-assets.ts
+++ components/games/sheep-tiles-assets.ts
@@ -13,7 +13,9 @@ import img10 from "./assets/羊了个素材/羊了个羊卡片素材_10_Neflibat
 import img11 from "./assets/羊了个素材/羊了个羊卡片素材_11_Neflibata_来自小红书网页版.jpg"
 import img12 from "./assets/羊了个素材/羊了个羊卡片素材_12_Neflibata_来自小红书网页版.jpg"
 import img13 from "./assets/羊了个素材/羊了个羊卡片素材_13_Neflibata_来自小红书网页版.jpg"
-
+import img14 from "./assets/羊了个素材/剪刀.jpg"
+import img15 from "./assets/羊了个素材/水桶.jpg"
+import img16 from "./assets/羊了个素材/铃铛.jpg"
 /** 与素材文件名编号1…13 对应，下标 0…12 */
 export const SHEEP_TILE_IMAGES: StaticImageData[] = [
   img01,
@@ -29,4 +31,7 @@ export const SHEEP_TILE_IMAGES: StaticImageData[] = [
   img11,
   img12,
   img13,
+  img14,
+  img15,
+  img16
 ]
